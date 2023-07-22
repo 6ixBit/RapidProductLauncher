b9eb75e874bca204ca2042b47bf5ed0f32d00350
psiddharthdesign <107192927+psiddharthdesign@users.noreply.github.com>
2023-07-22 23:31:31 +0530

    fixed popover on SidebarBottom

--- src/app/(authenticated-pages)/Sidebar/UserSidebar.tsx
+++ src/app/(authenticated-pages)/Sidebar/UserSidebar.tsx
@@ -51,7 +51,6 @@ export function UserSidebar({
         userFullname={userProfile.full_name ?? 'User'}
         isExpanded={isExpanded}
         userEmail={userEmail}
-        toggleIsExpanded={toggleIsExpanded}
       />
     </div>
   );
